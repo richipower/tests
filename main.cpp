17673f1f0bc48591a4bdf7a74b77b43c83667a1a
richi <rnava1020@hotmail.com>
2020-10-04 02:07:17 +0200

    Added opencv

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
+    Mat img = Mat::zeros(1,1,CV_8UC3);
+
 }
